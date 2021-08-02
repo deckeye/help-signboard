16e06bdacff09e3cc9a3dfa3c6062eb623bf85cb
Takuya Jumbo Nakamura <takedhy@gmail.com>
2021-08-02 16:52:05 +0900

    角柱版のコードのままだったので、再度修正。

--- main.ts
+++ main.ts
@@ -1,14 +1,7 @@
 input.onButtonPressed(Button.A, function () {
-    if (角度 < 180) {
-        角度 += 90
-    } else {
-        角度 = 0
-    }
-    pins.servoWritePin(AnalogPin.P0, 角度)
-    basic.pause(500)
+    pins.servoWritePin(AnalogPin.P0, 180)
 })
 input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P0, 0)
 })
-let 角度 = 0
 pins.analogSetPeriod(AnalogPin.P0, 20000)
